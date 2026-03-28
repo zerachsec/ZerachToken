8687ec7a6e9e95e64a3dfef953b63aba6484f5d1
zerachsec <zerach.sec@gmail.com>
2026-03-28 17:40:33 +0530

    fixed

--- script/DeployZKoToken.sol
+++ script/DeployZKoToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.28;
 
 import {Script} from "forge-std/Script.sol";
 import {ZerachOToken} from "src/ZerachOToken.sol";
@@ -7,9 +7,6 @@ import {ZerachOToken} from "src/ZerachOToken.sol";
 contract DeployZKoToken is Script {
 
     uint256 public constant INITIAL_SUPPLY = 1000 ether:
-    
-
-
     
     function run() external {
         vm.startBroadcast();
--- src/ZerachOToken.sol
+++ src/ZerachOToken.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.28;
 
 import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
-contract Zerachtoken is ERC20 {
+contract ZerachOToken is ERC20 {
     constructor(uint256 initialSuply) ERC20("zerachToken", "ZK") {
         _mint(msg.sender, initialSuply);
     }
